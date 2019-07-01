c999f75d57f50f20f9575a6fe0c6a1032a59ebf2
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-01 10:18:57 +0000

    Snapshot of branch master taken on 2019-07-01T06:18:56.687-04:00 from "https://github.com/UnofficialJuliaMirror/EcologicalNetworksPlots.jl-9f7a259d-73a7-556d-a7a2-3eb122d3865b"

---
+++ src/initial_layouts.jl
@@ -0,0 +1,44 @@
+"""
+Random disposition of nodes
+"""
+function initial(::Type{RandomInitialLayout}, N::T) where {T <: EcologicalNetworks.AbstractEcologicalNetwork}
+  return Dict([s => NodePosition() for s in species(N)])
+end
+
+"""
+Random disposition of nodes on two levels for bipartite networks
+"""
+function initial(::Type{BipartiteInitialLayout}, N::T) where {T <: EcologicalNetworks.AbstractBipartiteNetwork}
+  level = NodePosition[]
+  for (i, s) in enumerate(species(N))
+    this_level = s ∈ species(N; dims=1) ? 1.0 : 0.0
+    push!(level, NodePosition(rand(), this_level, 0.0, 0.0))
+  end
+  return Dict(zip(species(N), level))
+end
+
+"""
+Random disposition of nodes on trophic levels for food webs
+"""
+function initial(::Type{FoodwebInitialLayout}, N::T) where {T <: EcologicalNetworks.AbstractUnipartiteNetwork}
+  level = NodePosition[]
+  tl = fractional_trophic_level(N)
+  for (i, s) in enumerate(species(N))
+    push!(level, NodePosition(rand(), float(tl[s]), 0.0, 0.0))
+  end
+  return Dict(zip(species(N), level))
+end
+
+"""
+Random disposition of nodes on a circle
+"""
+function initial(::Type{CircularInitialLayout}, N::T) where {T <: EcologicalNetworks.AbstractEcologicalNetwork}
+  level = NodePosition[]
+  n = richness(N)
+  for (i, s) in enumerate(species(N))
+    θ = 2i * π/n
+    x, y = cos(θ), sin(θ)
+    push!(level, NodePosition(x, y, i))
+  end
+  return Dict(zip(species(N), level))
+end
